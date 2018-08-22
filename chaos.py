19932b6effb5e266a1f5450ad8ec87e7949ce254
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-22 01:28:33 -0500

    Loop prints indexes starting from end

--- chaos.py
+++ chaos.py
@@ -1,8 +1,9 @@
 print("New Year Chaos!")
 
 def min_bribes(array):
-
-	for i in range(0, len(array)):
-		if (array[i] - (i + 1) >= 3):
-			return "Too Chaotic!"
-print(min_bribes([1, 2, 3, 4, 8, 5, 7, 6]))		
+	for i in range(len(array) - 1, -1, -1):
+		print(i)
+		# If integer is 3 indexes or more off from original position in array
+		# if (e - (i + 1) >= 3):
+		# 	return "Too Chaotic!"
+min_bribes([1, 2, 3, 4, 8, 5, 7, 6])
