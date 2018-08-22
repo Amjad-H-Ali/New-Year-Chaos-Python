280b9634867ef595e419dade3d865c7c33373311
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-21 23:01:08 -0500

    Function returns if final state of array is impossible to be accomplished with the requirements

--- chaos.py
+++ chaos.py
@@ -3,5 +3,6 @@ print("New Year Chaos!")
 def min_bribes(array):
 
 	for i in range(0, len(array)):
-		print(array[i])
-min_bribes([1, 2, 3, 4])		
+		if (array[i] - (i + 1) >= 3):
+			return "Too Chaotic!"
+print(min_bribes([1, 2, 3, 4, 8, 5, 7, 6]))		
