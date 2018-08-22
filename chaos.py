0c37a55981adba90fd29a45baa941e9eac990a96
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-22 01:32:51 -0500

    Defined variable count that will store number of bribes

--- chaos.py
+++ chaos.py
@@ -1,9 +1,14 @@
 print("New Year Chaos!")
 
 def min_bribes(array):
+	counter = 0
 	for i in range(len(array) - 1, -1, -1):
 		print(i)
-		# If integer is 3 indexes or more off from original position in array
-		# if (e - (i + 1) >= 3):
-		# 	return "Too Chaotic!"
+		# If integer is 3 indexes or more away from original position in array
+		if (e - (i + 1) >= 3):
+			return "Too Chaotic!"
+
+			
 min_bribes([1, 2, 3, 4, 8, 5, 7, 6])
+
+min_bribes([1, 2, 3, 4, 5, 8, 6, 7])
